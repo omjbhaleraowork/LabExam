e6a19fcbbf006b21bb537032892172375e850609
om <omjbhaleraowork.com>
2023-07-18 23:53:40 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,121 @@
+﻿using LabExam.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabExam.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            List<Product> lstprod = Product.GetAllProducts();
+            return View(lstprod);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            Product e = Product.GetSingleProduct(ViewBag.Id = id);
+            return View(e);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product prod)
+        {
+            try
+            {
+                int count = Product.InserProduct(prod);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View();
+                }
+
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Product emp = Product.GetSingleProduct(ViewBag.Id = id);
+            return View(emp);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product prod)
+        {
+            try
+            {
+                int count = Product.Update(prod);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.message("Failed To Update");
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return View();
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Product emp = Product.GetSingleProduct(ViewBag.Id = id);
+            return View(emp);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                int count = Product.Delete(ViewBag.Id = id);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.alert("Failed to delete");
+                    return RedirectToAction(nameof(Delete));
+
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace LabExam.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+        public decimal Rate { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+
+        public static List<Product> GetAllProducts()
+        {
+            List<Product> prodLst = new List<Product>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LabExam;Integrated Security=True";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Products";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Product e = new Product();
+                    e.ProductId = dr.GetInt32("ProductId");
+                    e.ProductName = dr.GetString("ProductName");
+                    e.Rate = dr.GetDecimal("Rate");
+                    e.Description = dr.GetString("Description");
+                    e.CategoryName = dr.GetString("CategoryName");
+
+                    prodLst.Add(e);
+
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return prodLst;
+        }
+
+        public static Product GetSingleProduct(int ProdID)
+        {
+            Product? e = new Product();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LabExam;Integrated Security=True";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Products where ProductId=@ProdID";
+
+                cmd.Parameters.Add("@ProdID", SqlDbType.Int).Value = ProdID;
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+
+                    e.ProductId = dr.GetInt32("ProductId");
+                    e.ProductName = dr.GetString("ProductName");
+                    e.Rate = dr.GetDecimal("Rate");
+                    e.Description = dr.GetString("Description");
+                    e.CategoryName = dr.GetString("CategoryName");
+                }
+                else
+                {
+                    e = null;
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return e;
+        }
+
+        public static int Update(Product obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LabExam;Integrated Security=True";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "UpdateProduct";
+
+                cmd.Parameters.AddWithValue("@ProductId", obj.ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", obj.ProductName);
+                cmd.Parameters.AddWithValue("@Rate", obj.Rate);
+                cmd.Parameters.AddWithValue("@Description", obj.Description);
+                cmd.Parameters.AddWithValue("@CategoryName", obj.CategoryName);
+
+                int Status = cmd.ExecuteNonQuery();
+
+                if (Status > 0)
+                {
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return 0;
+        }
+
+        public static int Delete(int ProdID)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LabExam;Integrated Security=True";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "DeleteProduct";
+
+                cmd.Parameters.AddWithValue("@ProductId", ProdID);
+
+                int Status = cmd.ExecuteNonQuery();
+
+
+                if (Status > 0)
+                {
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return 0;
+        }
+
+        public static int InserProduct(Product obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LabExam;Integrated Security=True";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "InsertProduct";
+
+                cmd.Parameters.AddWithValue("@ProductId", obj.ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", obj.ProductName);
+                cmd.Parameters.AddWithValue("@Rate", obj.Rate);
+                cmd.Parameters.AddWithValue("@Description", obj.Description);
+                cmd.Parameters.AddWithValue("@CategoryName", obj.CategoryName);
+
+
+                int cnt = cmd.ExecuteNonQuery();
+
+                if (cnt > 0)
+                {
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return 0;
+        }
+    }
+
+}
